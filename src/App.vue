b056587fa7f4b1df2a09c491989b552f995988fb
amazingwebdev <lifesaverman25@gmail.com>
2018-10-25 11:42:57 -0600

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,191 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="position: relative">
+      <div ref="updateHTML" class="update-html"></div>
+      <textarea
+        id="message"
+        class="main-text-container"
+        v-model="mainText"
+        @input="onChangeText"
+        @keydown.up="onKeyUp"
+        @keydown.down="onKeyDown"
+        @keydown.enter="onKeyEnter"
+      ></textarea>
+      <auto-complete-modal
+        ref="autoCompleteModal"
+        :filtered-keywords="filteredKeywords"
+        :selected-index="selectedKeyIndex"
+        @autoComplete="autoComplete"
+        class="modal-wrapper"
+        :style="{visibility: `${isVisibleModal ? 'visible' : 'hidden'}`}"
+      ></auto-complete-modal>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AutoCompleteModal from './components/AutoCompleteModal.vue';
+import getCaretCoordinates from 'textarea-caret';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AutoCompleteModal
+  },
+  computed: {
+    filteredKeywords () {
+      this.selectedKeyIndex = -1;
+      return this.keywords.filter(keyword => this.search.startsWith('{{') && keyword.key.toLowerCase().includes(this.search.toLowerCase()));
+    }
+  },
+  data () {
+    return {
+      keywords: [
+        {
+          key: '{{First Name}}',
+          color: 'blue'
+        },
+        {
+          key: '{{Email}}',
+          color: 'blue'
+        },
+        {
+          key: '{{Gender2}}',
+          color: 'green'
+        },
+        {
+          key: '{{Age}}',
+          color: 'green'
+        }
+      ],
+      search: '',
+      mainText: '',
+      selectedKeyIndex: -1,
+      isVisibleModal: false
+    }
+  },
+  methods: {
+    onChangeText (event) {
+      const strings = this.mainText.split(' ');
+      const endPosition = getCaretCoordinates(event.target, event.target.selectionEnd);
+
+      this.search = strings[strings.length - 1];
+      this.$refs.autoCompleteModal.$el.style.cssText = `position: absolute; left: ${endPosition.left}px; top: ${endPosition.top}px;`;
+
+      if(!this.filteredKeywords.length || !this.mainText) {
+        this.$refs.autoCompleteModal.$el.style.visibility = 'hidden';
+      }
+
+      this.updateHTML();
+    },
+    updateHTML () {
+      let mainTextCopy = this.mainText;
+
+      String.prototype.replaceAll = function(search, replacement) {
+        let target = this;
+        return target.replace(new RegExp(search, 'g'), replacement);
+      };
+
+      this.keywords.forEach(keyword => {
+        const replacement = `<span style="background-color: ${keyword.color}; color: white;">${keyword.key}</span>`;
+
+        mainTextCopy = mainTextCopy.replaceAll(keyword.key, replacement);
+      });
+
+      this.$refs.updateHTML.innerHTML = mainTextCopy;
+    },
+    autoComplete (selectedText) {
+      const lastReplacePosition = this.mainText.lastIndexOf(this.search);
+
+      this.mainText = this.mainText.substring(0, lastReplacePosition) + selectedText;
+      this.$refs.autoCompleteModal.$el.style.visibility = 'hidden';
+      document.getElementById('message').focus();
+
+      this.updateHTML();
+    },
+    onKeyUp (event) {
+      if (this.filteredKeywords.length) {
+        event.preventDefault();
+
+        switch (this.selectedKeyIndex) {
+          case 0:
+          case -1:
+            this.selectedKeyIndex = this.filteredKeywords.length - 1;
+            break;
+          default:
+            this.selectedKeyIndex = this.selectedKeyIndex - 1;
+        }
+      }
+    },
+    onKeyDown (event) {
+      if (this.filteredKeywords.length) {
+        event.preventDefault();
+
+        this.selectedKeyIndex = (this.selectedKeyIndex + 1) % this.filteredKeywords.length;
+      }
+    },
+    onKeyEnter (event) {
+      if (this.selectedKeyIndex >= 0) {
+        event.preventDefault();
+
+        this.autoComplete(this.filteredKeywords[this.selectedKeyIndex].key);
+        this.selectedKeyIndex = -1;
+
+        this.updateHTML();
+      }
+    }
+  },
+  mounted () {
+    this.$refs.autoCompleteModal.$el.style.visibility = 'hidden';
+    document.getElementById('message').focus();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .main-text-container {
+    width: 600px;
+    height: 400px;
+    font-size: 25px;
+    color: transparent;
+    font-family: "Times New Roman";
+    background-color: transparent !important;
+    caret-color: black;
+  }
+
+  .update-html {
+    position: absolute;
+
+    text-rendering: auto;
+    color: initial;
+    letter-spacing: normal;
+    word-spacing: normal;
+    text-transform: none;
+    text-indent: 0;
+    text-shadow: none;
+    display: inline-block;
+    text-align: start;
+    margin: 0;
+    font: 400 25px Times New Roman;
+
+    -webkit-rtl-ordering: logical;
+    flex-direction: column;
+    cursor: text;
+    white-space: pre-wrap;
+    overflow-wrap: break-word;
+    padding: 2px;
+  }
+
+  .modal-wrapper {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
 </style>
---
+++ src/components/AutoCompleteModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="auto-complete" @keydown.38="moveSelection" @keydown.40="moveSelection">
+    <ul>
+      <li
+          v-for="(keyword, index) in filteredKeywords"
+          :style="{backgroundColor: `${selectedIndex === index ? 'gray' : 'transparent'}`}"
+          @click="onSelect"
+      >
+        <span :style="{backgroundColor: `${keyword.color}`, whiteSpace: 'noWrap'}">{{keyword.key}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AutoCompleteModal',
+  props: {
+    filteredKeywords: Array,
+    selectedIndex: Number
+  },
+  methods: {
+    onSelect (event) {
+      this.$emit('autoComplete', event.target.innerText);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  ul li {
+    color: white;
+    padding: 5px 10px;
+  }
+
+  .auto-complete {
+    max-width: 200px;
+    padding: 0;
+    border: 1px solid gray;
+    background-color: white;
+  }
+</style>
